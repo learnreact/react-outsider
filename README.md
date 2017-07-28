990d9774f3639dc06ea111d2258955e4dffaa0a0
Michael Chan <mijoch@gmail.com>
2017-07-28 10:26:31 -0700

    start: copy/paste from project source

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # react-outsider
 A component for observing clicks outside an element
+
+## WIP
+
+## Usage
+```js
+import Outsider from "react-outsider";
+
+export default class extends React.Component {
+  constructor() {
+    super();
+    this.state = { contentShown: false }
+  }
+
+  render() {
+    return this.state.contentShown ? (
+      <Outsider
+        onOutsideClick={
+          this.setState(({ contentShown }) => ({
+            contentShown: !contentShown
+          }))
+        }
+      >
+        Some content
+      </Outsider>
+    ) : (
+      <div />
+    )
+  }
+}
+```
+
+## License
+MIT, Michael Chan
---
+++ index.js
@@ -0,0 +1,27 @@
+export default class extends Component {
+  constructor() {
+    super()
+    this.handleOutsideClick = this.handleOutsideClick.bind(this)
+  }
+
+  handleOutsideClick(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    if (this.container.contains(e.target)) return
+
+    return this.props.onOutsideClick()
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleOutsideClick, true)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleOutsideClick, true)
+  }
+
+  render() {
+    return <div ref={c => (this.container = c)} {...props} />
+  }
+}
